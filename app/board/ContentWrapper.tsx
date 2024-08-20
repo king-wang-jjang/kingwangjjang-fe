7d5ea19496585b2da2bf6ab5ad3977db5ca6d95b
hwanju <hwanju1596@gmail.com>
2024-08-20 22:27:06 +0900

    💄 like ui 추가

--- app/board/ContentWrapper.tsx
+++ app/board/ContentWrapper.tsx
@@ -1,91 +1,125 @@
-'use client'
-import { Box, useMediaQuery, useTheme } from "@mui/material";
-import { PostList } from "@/components/Post/PostList";
-import { SummaryBoardDocument, RealtimePaginationQuery } from "@/gql/graphql";
-import { useMutation, useQuery } from "@apollo/client";
-import Grid from "@mui/material/Unstable_Grid2/Grid2";
-import { SetStateAction, useEffect, useRef, useState } from "react";
-import { Loading } from "./Loading";
-import { Error } from "./Error";
-import { RealtimePost } from "@/components/Post/RealtimePost";
-import { Filter } from "./Filter";
-import { FilterCollectionType } from "@/types/board-type";
-import useInfiniteScrollablePostList from "../hooks/useInfiniteScrollablePostList";
+import { PostCardType } from "@/app/types/board-type";
+import {
+  Box,
+  Card,
+  CardContent,
+  Collapse,
+  Tooltip,
+  Typography,
+  useMediaQuery,
+  useTheme,
+  IconButton,
+} from "@mui/material";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+import LaunchIcon from "@mui/icons-material/Launch";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import Label from "@/app/components/UI/Label";
+import { theme } from "@/app/styles/theme";
+import anime from 'animejs/lib/anime.es.js';
 
-export const ContentWrapper = () => {
+interface Props extends PostCardType {
+  onClickToggle: (boardId: string, site: string) => void;
+}
+
+export const PostCard = ({ id, site, title, url, createTime, GPTAnswer, rank, onClickToggle, }: Props) => {
+  const [expanded, setExpanded] = useState(false);
+  const [liked, setLiked] = useState(false);
+  const cardRef = useRef(null); 
+  const [isHovering, setIsHovering] = useState(false);
   const pageTheme = useTheme();
-  const [postData, setPostData] = useState<RealtimePaginationQuery['realtimePagination']>();
-  const [fiteredPostData, setFiteredPostData] = useState<RealtimePaginationQuery['realtimePagination']>();
-  const isMobile = useMediaQuery(pageTheme.breakpoints.down("xs"));
-  const [filterCollection, setFilterCollection] = useState<FilterCollectionType>();
-  const {loadingRef, loading: boardContentsQueryLoading, error: boardContentsQueryError, data: boardContentsData} 
-  = useInfiniteScrollablePostList();
+  const isMobile = useMediaQuery(pageTheme.breakpoints.down("sm"));
 
-  const [ summaryBoardMutation, { data: summaryBoardMutationData, loading: summaryBoardMutationLoading, error: summaryBoardMutationError,},]
-  = useMutation(SummaryBoardDocument, { refetchQueries: ["BoardContentsByDate"] });
-  
-  const handleSummaryBoard = (boardId: string, site: string) => {
-    summaryBoardMutation({
-      variables: { boardId: boardId, site: site },
-      refetchQueries: ['BoardContentsByDate'],
-      async onQueryUpdated(observableQuery) {
-          await observableQuery.refetch();
-      },
-    });
-  }
-
-  useEffect(()=>{
-    // Site이름을 영어 -> 한글
-    const modifiedData = (boardContentsData?.realtimePagination)?.map((value) => {
-      if (value?.site === "ygosu") {
-        return { ...value, site: "와이고수" };
-      } else if (value?.site === "dcinside") {
-        return { ...value, site: "디시인사이드" };
-      } else if (value?.site === "ppomppu") {
-        return { ...value, site: "뽐뿌" };
-      }
-      return value;
-    });
-
-    setPostData(modifiedData);
-    setFiteredPostData(modifiedData);
-
-    const uniqueSite = Array.from(new Set(
-      modifiedData
-        ? modifiedData.map((item) => item?.site).filter(site => typeof site === 'string').map(String)
-        : []
-    )); 
-    
-    if (uniqueSite !== undefined) {
-      setFilterCollection({ site: uniqueSite });
+  const handleMouseOver = () => {
+    if(!isMobile){
+      setIsHovering(true);
     }
-  },[boardContentsData])
+  };
 
-  if(isMobile) return boardContentsData?.realtimePagination && <PostList onClickCard={handleSummaryBoard} postItems={postData ?? []} />
-  
-  return(
-    <>
-    <Grid container spacing={2} margin={0} paddingY="0"
-          position="relative" >
-      <Grid xs={0} md={3} paddingY="0" > 
-        {/* 왼쪽 Side */}
-        <Box width="100%" bgcolor="white" position="sticky" top="73px" >
-          <Filter setFilteredPostData={setFiteredPostData} postData={postData} filteredData={filterCollection} />
-        </Box>
-      </Grid>
-      <Grid xs={12} md={6}>
-        <PostList onClickCard={handleSummaryBoard} postItems={fiteredPostData ?? []} />
-        {boardContentsQueryLoading && <Loading />}
-        {boardContentsQueryError && <Error message={boardContentsQueryError.message} isMobile={isMobile} />}
-        <div ref={loadingRef}/>
-      </Grid>
-      <Grid xs={0} md={3} paddingY="0" >
-        {/* 오른쪽 Side */}
-        <Box width="100%" bgcolor="white" position="sticky" top="73px" >
-          <RealtimePost/>
-        </Box>
-      </Grid>
-    </Grid>
-   </>
+  const handleMouseOut = () => {
+    if(!isMobile){
+      setIsHovering(false);
+    }
+  };
+
+  useEffect(() => {
+    const cardElement = cardRef.current;
+    if (cardElement) {
+      anime({
+        targets: cardElement,
+        translateX: isHovering ? -50 : 0,
+        duration: 300,
+        easing: 'easeInOutQuad', 
+      });
+    }
+  }, [isHovering]);
+
+  const handleToggle = (boardId: string, site: string) => {
+    setExpanded(!expanded);
+    if (!expanded) {
+      onClickToggle(boardId, site); 
+    }
+  };
+
+  const handleLikeClick = (e: React.MouseEvent) => {
+    e.stopPropagation(); // Prevent triggering the card click event
+    setLiked(!liked); // Toggle the liked state
+  };
+
+  const isNotRealtime: boolean = rank !== null;
+  return (
+    <Box position="relative" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+      <Card ref={cardRef} className={'mb-3'} sx={{ width: "100%", zIndex:"100", position:"relative" }} onClick={ () => handleToggle(id, site) }>
+        <CardContent sx={{transform: 'none', display: "flex", flexDirection: "column", position: "relative" }}>
+          <Box>
+            <Label label={site} bgcolor={theme.chip.site} />
+            {isNotRealtime && <Label label={rank} bgcolor={theme.chip.rank}/>}
+          </Box>
+          <Box sx={{display: "flex", justifyContent: "space-between", padding: "5px 0px", gap: "5px"}}>
+            <Tooltip title={String(createTime)} arrow>
+              <Typography variant="body2" component="div">
+                {title}
+              </Typography>
+              
+            </Tooltip>
+            {isMobile && 
+              <Link href={url} target="_blank"passHref onClick={(e) => e.stopPropagation()}>
+                <LaunchIcon />
+              </Link>
+              }
+          </Box>
+          <Box>
+            <Collapse in={expanded} timeout="auto" > {/*unmountOnExit*/}
+              <Box sx={{ display: "flex", justifyContent: "space-between", 
+                      alignItems: "center",padding: "5px 0px",}}>
+                <Typography variant="body2" component="div"
+                  sx={{ whiteSpace: "pre-wrap", wordBreak: "break-word",}}>
+                  {GPTAnswer}
+                </Typography>
+              </Box>
+            </Collapse>
+          </Box>
+          {/* Like Button UI */}
+          <IconButton
+            onClick={handleLikeClick}
+            sx={{
+              position: "absolute",
+              bottom: 8,
+              left: 8,
+              color: liked ? "red" : "gray",
+            }}
+          >
+            <FavoriteIcon />
+          </IconButton>
+        </CardContent>
+      </Card>
+      <Box sx={{position:"absolute", top:"0", borderRadius:"5px", left:"0", bgcolor:"#3b82f6",width: "100%", height:"100%", boxShadow:"none"}}>
+        <Link href={url} target="_blank" passHref onClick={(e) => e.stopPropagation()}>
+          <Box width="100%" height="100%" display="flex" alignItems="center" justifyContent="end">
+            <LaunchIcon sx={{width:"50px", color:"white"}}/>
+          </Box>
+        </Link>
+      </Box>
+    </Box>
   );
 };
