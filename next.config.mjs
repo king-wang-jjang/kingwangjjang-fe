4c5445b64704b752916b0f386f0231b370f16cbf
hwanju <hwanju1596@gmail.com>
2024-09-10 21:57:54 +0900

    💥 eslint!

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,37 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const isStaticExport = 'false';
+
 const nextConfig = {
-    async rewrites() {
-        return [
-          {
-            source: '/board',
-            destination: '/', // 페이지를 변경하지 않고 URL만 수정합니다
-          },
-        ];
-      },
-  };
+  trailingSlash: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  env: {
+    BUILD_STATIC_EXPORT: isStaticExport,
+  },
+  modularizeImports: {
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+    },
+    '@mui/lab': {
+      transform: '@mui/lab/{{member}}',
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  ...(isStaticExport === 'true' && {
+    output: 'export',
+  }),
+};
 
 export default nextConfig;
