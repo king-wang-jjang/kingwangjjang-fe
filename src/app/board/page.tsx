63396807ba1cebfb29aa687ca67fb1c809b15f7a
hwanju <hwanju1596@gmail.com>
2025-01-24 02:35:28 +0900

    Merge remote-tracking branch 'origin/develop'

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -2,7 +2,7 @@ import { CONFIG } from 'src/config-global';
 
 import { BoardView } from '../../sections/board/view/board-view';
 
-export const metadata = { title: `Issue - ${CONFIG.appName}` };
+export const metadata = { title: `실시간 인기 - ${CONFIG.appName}` };
 
 export default function Page() {
   return <BoardView />;
