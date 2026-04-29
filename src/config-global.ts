241f0d3f9e18950ec9fdf51e5bd2bfc2d122ba99
hwanju <hwanju1596@gmail.com>
2026-04-30 00:22:08 +0900

    CI: harden frontend env deployment

--- src/config-global.ts
+++ src/config-global.ts
@@ -11,7 +11,7 @@ export type ConfigValue = {
 export const CONFIG: ConfigValue = {
   appName: 'Kingwangjjang',
   appVersion: packageJson.version,
-  serverUrl: process.env.NEXT_PUBLIC_SERVER_URL ?? 'https://api.마약.kr',
-  imageServerUrl: process.env.NEXT_PUBLIC_IMAGE_SERVER_URL ?? 'https://images.마약.kr',
+  serverUrl: process.env.NEXT_PUBLIC_SERVER_URL || 'https://api.마약.kr',
+  imageServerUrl: process.env.NEXT_PUBLIC_IMAGE_SERVER_URL || 'https://images.마약.kr',
   isStaticExport: JSON.parse(process.env.NEXT_PUBLIC_BUILD_STATIC_EXPORT ?? 'false'),
 };
